c84db965c3269b1cd751229bd78d9416124d6d71
asi-isa <ali.inceoglu@protonmail.com>
2021-12-02 14:10:15 +0100

    details

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b5ec7fd2fddeae3"; }
+/******/ 		__webpack_require__.h = function() { return "6e66c4ff5f07dd74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- comps/home/LPMobile.js
+++ comps/home/LPMobile.js
@@ -13,7 +13,7 @@ export default function LPMobile() {
         params={{
           particles: {
             number: {
-              value: 94,
+              value: 120,
               density: {
                 enable: false,
               },
