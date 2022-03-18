cf9b1a36b8b0b8f9fe14e4b251903cb031af9aae
asi-isa <ali.inceoglu@protonmail.com>
2022-03-18 14:31:04 +0100

    .

--- pages/dataprivacy.js
+++ pages/dataprivacy.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function dataprivacy() {
   return (
     <div>
-      <p>Your data is solely stored on your device.</p>
+      <p style={{ color: "#eee" }}>
+        Your data is solely stored on your device.
+      </p>
       <p></p>
     </div>
   );
