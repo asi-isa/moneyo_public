20e99cc712684f9ecb0ff615411989a2e1702689
asi-isa <ali.inceoglu@protonmail.com>
2022-03-18 14:29:01 +0100

    dataprivacy

---
+++ pages/dataprivacy.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function dataprivacy() {
+  return (
+    <div>
+      <p>Your data is solely stored on your device.</p>
+      <p></p>
+    </div>
+  );
+}
